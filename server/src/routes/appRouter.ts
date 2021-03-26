726d5179119b8eaeeb63190cf6983b4f51da0ccf
MattMan569 <matthewpolsom@hotmail.com>
2021-03-26 01:03:05 -0600

    remove api from redirection route

--- server/src/routes/appRouter.ts
+++ server/src/routes/appRouter.ts
@@ -4,9 +4,10 @@ import appController from './controllers/appController';
 export const appRouter = Router();
 
 appRouter.get('/', appController.client);
+appRouter.get('/:id', appController.redirect);
+
 appRouter.get('/api', appController.apiInformation);
 appRouter.get('/api/url/:id', appController.urlInformation);
-appRouter.get('/api/:id', appController.redirect);
 
 appRouter.post('/api/url', appController.createUrl);
 
