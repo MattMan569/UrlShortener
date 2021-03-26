bf9da35de228978b0e0a45a73916ef98c76d9fa1
MattMan569 <matthewpolsom@hotmail.com>
2021-03-26 01:03:16 -0600

    fix errant https

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -7,7 +7,17 @@ import { appRouter } from './../routes/appRouter';
 
 export const app = express();
 
-app.use(helmet());
+// NOTE: change if server gets https
+// Remove the upgrade-insecure-requests header to prevent https resource requests
+const { 'upgrade-insecure-requests': discard, ...csp } = helmet.contentSecurityPolicy.getDefaultDirectives();
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            ...csp,
+        },
+    },
+}));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
